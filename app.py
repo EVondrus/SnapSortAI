443e4d17307f2e001035029588b28f793a349bc1
Ellenor Olofsson <ellavondrus@gmail.com>
2024-09-19 12:56:37 +0000

    chore: Add app pages and load scripts

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-#from app_pages.visualizer import page_visualizer_body
+from app_pages.visualizer import page_visualizer_body
+#from app_pages.model_performance import page_ml_performance_body
+#from app_pages.object_detector import page_object_detector_body
 from app_pages.hypothesis import page_hypothesis_body
 
 app = MultiPage(app_name= "SnapSortAI") # Create an instance of the app 
 
 # Add app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
-#app.add_page("Data Visualizer",  page_visualizer_body)
 app.add_page("Hypothesis Statement", page_hypothesis_body)
+app.add_page("Data Visualizer", page_visualizer_body)
+#app.add_page("Model Performance", page_ml_performance_body)
+#app.add_page("Object Identification", page_object_detector_body)
+
 
 app.run()
