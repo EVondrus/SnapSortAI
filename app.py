c6390d0aaffa81e13daff94a9856e4ee7cfcde33
Ellenor Olofsson <ellavondrus@gmail.com>
2024-09-19 13:46:34 +0000

    chor: Add object_detector page and script

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.visualizer import page_visualizer_body
 from app_pages.model_performance import page_ml_performance_body
-#from app_pages.object_detector import page_object_detector_body
+from app_pages.object_detector import page_object_detector_body
 from app_pages.hypothesis import page_hypothesis_body
 
 app = MultiPage(app_name= "SnapSortAI") # Create an instance of the app 
@@ -15,7 +15,7 @@ app.add_page("Project Summary", page_summary_body)
 app.add_page("Hypothesis Statement", page_hypothesis_body)
 app.add_page("Data Visualizer", page_visualizer_body)
 app.add_page("Model Performance", page_ml_performance_body)
-#app.add_page("Object Identification", page_object_detector_body)
+app.add_page("Object Identification", page_object_detector_body)
 
 
 app.run()
