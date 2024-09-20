2b376be5b6861a18173631b169b6c9da25577498
Ellenor Olofsson <ellavondrus@gmail.com>
2024-09-20 22:35:50 +0000

    refactor: Update code to PEP8 standard

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def page_hypothesis_body():
 
     st.write('---')
@@ -9,57 +10,67 @@ def page_hypothesis_body():
     st.write('---')
 
     st.info('''
-**Hypothesis 1:** Exploratory Data Analysis (EDA) will reveal patterns and challenges that will guide preprocessing and model development.
+**Hypothesis 1:** Exploratory Data Analysis (EDA) will reveal patterns and\
+challenges that will guide preprocessing and model development.
 ''')
 
     st.warning('''
-**Validation Method:** Conduct Exploratory Data Analysis on the CIFAR-10 dataset, including visualizing class distribution and image quality.
+**Validation Method:** Conduct Exploratory Data Analysis on the\
+CIFAR-10 dataset, including visualizing class distribution and image quality.
 ''')
 
     st.success('''
-**Conclusion:** EDA effectively identified class imbalances and image quality issues, guiding preprocessing steps to improve model training. **Hypothesis confirmed**.
+**Conclusion:** EDA effectively identified class imbalances and image quality\
+issues, guiding preprocessing steps to improve model training.\
+**Hypothesis confirmed**.
 ''')
 
     st.write('---')
 
     st.info('''
-**Hypothesis 2:** A basic CNN will achieve at least 70% classification accuracy.
+**Hypothesis 2:** A basic CNN will achieve at least\
+70% classification accuracy.
 ''')
 
     st.warning('''
-**Validation Method:** Train a Convolutional Neural Network (CNN) and evaluate its performance on the test set.
+**Validation Method:** Train a Convolutional Neural Network (CNN) and evaluate\
+its performance on the test set.
 ''')
 
     st.success('''
-**Conclusion:** 
+**Conclusion:**
 ''')
 
     st.write('---')
 
     st.info('''
-**Hypothesis 3:** Data augmentation (e.g., rotating, flipping, zooming) will enhance model accuracy by at least 5%.
+**Hypothesis 3:** Data augmentation (e.g., rotating, flipping, zooming) will\
+enhance model accuracy by at least 5%.
 ''')
 
     st.warning('''
-**Validation Method:** Compare model performance with and without data augmentation.
+**Validation Method:** Compare model performance with and\
+without data augmentation.
 ''')
 
     st.success('''
-**Conclusion:** 
+**Conclusion:**
 ''')
 
     st.write('---')
 
     st.info('''
-**Hypothesis 4:** Fine-tuning hyperparameters will significantly enhance model accuracy.
+**Hypothesis 4:** Fine-tuning hyperparameters will significantly\
+enhance model accuracy.
 ''')
 
     st.warning('''
-**Validation Method:** Compare model performance with default hyperparameters versus tuned hyperparameters.
+**Validation Method:** Compare model performance with default\
+hyperparameters versus tuned hyperparameters.
 ''')
 
     st.success('''
-**Conclusion:** 
+**Conclusion:**
 ''')
 
     st.write('---')
@@ -69,11 +80,12 @@ def page_hypothesis_body():
 ''')
 
     st.warning('''
-**Validation Method:** Train and evaluate the model on datasets with varying image resolutions.
+**Validation Method:** Train and evaluate the model on datasets\
+with varying image resolutions.
 ''')
 
     st.success('''
-**Conclusion:** 
+**Conclusion:**
 ''')
 
     st.write('---')
