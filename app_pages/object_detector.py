f9dbca4950f0d7578b91822d01f765afe9db4a56
Ellenor Olofsson <ellavondrus@gmail.com>
2024-09-20 22:30:48 +0000

    refactor:Add B requirements 2
    update code to PEP8 standard

--- app_pages/object_detector.py
+++ app_pages/object_detector.py
@@ -15,24 +15,24 @@ def page_object_detector_body():
 
     st.write('---')
 
-    st.header('**Image classifyer**')
+    st.header('**Image classifier**')
 
-    st.info('''
-    Business Requirement 2: 
-    ''')
-
-    st.info('''
-    Business Requirement 3: 
-    ''')
+    st.info('**Business Requirement 2:** This feature allows clients to upload\
+    images from various classes and receive real-time predictions.\
+    It automates the classification process, providing immediate feedback on\
+    the models predictions and associated probabilities.')
 
     st.warning('''
-    Click [**here**](https://github.com/EVondrus/SnapSortAI/blob/main/outputs/sample_images.zip),
+    Click [**here**](
+    https://github.com/EVondrus/SnapSortAI/blob/main/outputs/sample_images.zip
+    ),
     to download a set of images from each class for live prediction.''')
 
-    images_buffer = st.file_uploader('Upload images from the different classes here,\
-        You can select more than one at the time.',
-            type = ['png', 'jpg'],
-            accept_multiple_files = True)
+    images_buffer = st.file_uploader(
+        'Upload images from the different classes here, You can select\
+        more than one at the time.', type=['png', 'jpg'],
+        accept_multiple_files=True
+    )
 
     if images_buffer is not None:
         df_report = pd.DataFrame([])
@@ -56,7 +56,6 @@ def page_object_detector_body():
             df_report = pd.concat([df_report, pd.DataFrame(
                 {'Name': [image.name], 'Result': [predict_class]}
                 )], ignore_index=True)
-
 
         if not df_report.empty:
             st.warning('Analysis Report')
