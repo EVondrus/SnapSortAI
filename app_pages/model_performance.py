fbd8b29efa1aa3c018cb7a40c06a2e7da37549ba
Ellenor Olofsson <ellavondrus@gmail.com>
2024-09-20 13:14:18 +0000

    feat: Add model train history section
    and update path to spurce

--- app_pages/model_performance.py
+++ app_pages/model_performance.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
-#from src.machine_learning.evaluate_clf import load_test_evaluation
+from source.machine_learning.evaluate_clf import load_test_evaluation
 
 def page_ml_performance_body():
 
@@ -20,3 +20,17 @@ def page_ml_performance_body():
              caption='Labels Distribution on Train, Validation and Test Sets')
 
     st.write("---")
+
+    st.success("Model training history")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+    st.warning("Generalised performance on the test set")
+    st.dataframe(pd.DataFrame(
+        load_test_evaluation(version), index=['Loss', 'Accuracy']))
